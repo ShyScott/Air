be6c438761703695e39bbe3bbc5f70805fcf13bc
Michael Long <mchobbylong@gmail.com>
2020-04-25 12:55:32 +0800

    remove TokenRefresh view & modify api url prefix

--- Src/backend/backend/urls.py
+++ Src/backend/backend/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from rest_framework_simplejwt.views import TokenObtainPairView
 
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
@@ -35,8 +35,7 @@ schema_view = get_schema_view(
 urlpatterns = [
     path('djangoadmin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
-    path('api-token/get/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api-token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('swagger-ui/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('', include('tcas.urls')),
+    path('api/', include('tcas.urls')),
 ]
