6097a5c00c0e194f891815dacc970ee138c0483e
Michael Long <mchobbylong@gmail.com>
2020-04-27 16:27:01 +0800

    add default pagination class

--- Src/backend/backend/settings.py
+++ Src/backend/backend/settings.py
@@ -147,6 +147,8 @@ REST_FRAMEWORK = {
         'django_filters.rest_framework.DjangoFilterBackend',
         'rest_framework.filters.OrderingFilter',
     ],
+    'DEFAULT_PAGINATION_CLASS': 'tcas.utils.ExtendedPageNumberPagination',
+    'PAGE_SIZE': 10,
 }
 
 # Swagger in DRF (drf-yasg) settings
