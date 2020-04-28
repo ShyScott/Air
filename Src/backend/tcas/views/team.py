330db66a2930b199a6518ed99fe07227e196cdc4
Michael Long <mchobbylong@gmail.com>
2020-04-29 00:14:40 +0800

    add validations for student creating teams

--- Src/backend/tcas/views/team.py
+++ Src/backend/tcas/views/team.py
@@ -2,6 +2,7 @@ from rest_framework import status
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.decorators import action
 from rest_framework.response import Response
+from rest_framework.exceptions import ValidationError
 
 from .generic import PermissionDictMixin
 from tcas.models import Team
@@ -22,6 +23,13 @@ class TeamViewSet(PermissionDictMixin, ModelViewSet):
         'rename': [IsTeacher | IsInCurrentCourse],
         'quit': [IsTeacher | IsInCurrentTeam],
     }
+
+    def perform_create(self, serializer):
+        user = self.request.user
+        if user.teams.filter(course=serializer.data['course']).exists():
+            raise ValidationError('This user is already in one team of the course!')
+        team = serializer.save()
+        team.members.add(self.request.user)
 
     @action(detail=True, methods=['put'], serializer_class=TeamNameSerializer)
     def rename(self, request, *arg, **kwargs):
