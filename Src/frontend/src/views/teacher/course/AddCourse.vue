a29798a7c834cf6d57c812a161d155be2e00bc4a
YUBIKIRI520 <648308255@qq.com>
2020-04-29 21:53:53 +0800

    Finish the file upload part

--- Src/frontend/src/views/teacher/course/AddCourse.vue
+++ Src/frontend/src/views/teacher/course/AddCourse.vue
@@ -42,6 +42,8 @@
                         :directory="false"
                         accept="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet,application/vnd.ms-excel"
                         action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
+                        :beforeUpload="checkIfFileExisted"
+                        :remove="removeFile"
                         @change="handleChange"
                         @reject="rejectFilesNotAccept"
                       >
@@ -67,6 +69,7 @@
 </template>
 
 <script>
+  import XLSX from 'xlsx'
   export default {
     name: 'AddCourse',
     data () {
@@ -86,7 +89,12 @@
         // Year of the duration
         durationY: '',
         // Semester of the duration
-        durationS: ''
+        durationS: '',
+        // Excel file upload
+        // if we need to transfer the file stream to binary - false
+        rABS: false,
+        wb: '',
+        tableData: []
       }
     },
     created () {
@@ -114,12 +122,23 @@
       handleChange (info) {
         const status = info.file.status
         if (status !== 'uploading') {
-          console.log(info, info.fileList)
+          // console.log(info, info.fileList)
         }
+        // if file uploading done
         if (status === 'done') {
-          this.$message.success(`${info.file.name} file uploaded successfully.`)
+          // if succeed
+          this.$notification.success({
+            message: 'Success',
+            description: `${info.file.name} is uploaded successfully`
+          })
+          // import the file content
+          this.importf(info.file)
         } else if (status === 'error') {
-          this.$message.error(`${info.file.name} file upload failed.`)
+          // if failing
+          this.$notification.error({
+            message: 'Error',
+            description: `${info.file.name} file upload failed.`
+          })
         }
       },
       // function used to reject files that not accept
@@ -128,6 +147,83 @@
           message: 'Warning',
           description: 'Only .xls .xlsx .csv files Accepted'
         })
+      },
+      // Teacher can only upload at most one excel file
+      checkIfFileExisted (file) {
+        return new Promise((resolve, reject) => {
+          // if file is already uploaded
+          if (this.tableData.length !== 0) {
+            this.$notification.error({
+              message: 'Error',
+              description: 'You can only upload one file'
+            })
+            // reject the file
+            // eslint-disable-next-line prefer-promise-reject-errors
+            return reject(false)
+          } else {
+            return resolve(true)
+          }
+        })
+      },
+      // function executed when teacher remove the file uploaded
+      removeFile () {
+        return new Promise((resolve, reject) => {
+          // reset the variable tableData
+          this.tableData = []
+          return resolve(true)
+        })
+      },
+      // function used to import file
+      importf (file) {
+        var _this = this
+        this.file = file
+        const extention = file.name.split('.')[1] === 'xls'
+        const extention2 = file.name.split('.')[1] === 'xlsx'
+        // console.log(extention,extention2,isLt5M)
+        if (!extention && !extention2) {
+          this.$message.warning('Please upload Excel file')
+          return false
+        }
+        // const data1 = XLSX.utils.sheet_to_json(file.name)
+
+        var f = file.originFileObj
+        var that = this
+        var reader = new FileReader()
+        reader.onload = function (e) {
+          // console.log(e)
+          let wb = that.wb
+          var data = e.target.result
+          if (that.rABS) {
+            wb = XLSX.read(btoa(that.fixdata(data)), {
+              // transfer
+              type: 'base64'
+            })
+          } else {
+            wb = XLSX.read(data, {
+              type: 'binary'
+            })
+          }
+          // wb.SheetNames[0] is to get the name of the first sheet
+          // wb.Sheets[Sheet's name] is to get the data of the first sheet
+          // document.getElementById("demo").innerHTML= JSON.stringify( XLSX.utils.sheet_to_json(wb.Sheets[wb.SheetNames[0]]) );
+          _this.tableData = XLSX.utils.sheet_to_json(wb.Sheets[wb.SheetNames[0]])
+          // _this.tableData = data1
+          // console.log(_this.tableData)
+        }
+        if (this.rABS) {
+          reader.readAsArrayBuffer(f)
+        } else {
+          reader.readAsBinaryString(f)
+        }
+      },
+      fixdata (data) {
+        // file stream transferred to binary string
+        var o = ''
+        var l = 0
+        var w = 10240
+        for (; l < data.byteLength / w; ++l) o += String.fromCharCode.apply(null, new Uint8Array(data.slice(l * w, l * w + w)))
+        o += String.fromCharCode.apply(null, new Uint8Array(data.slice(l * w)))
+        return o
       }
     }
   }
