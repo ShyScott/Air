6d0f8cf4f3c2c9ed073e9d98e8aa77c5508ab83f
Michael Long <mchobbylong@gmail.com>
2020-05-02 00:07:25 +0800

    allow teachers to edit team members directly

--- Src/backend/tcas/serializers/team.py
+++ Src/backend/tcas/serializers/team.py
@@ -6,6 +6,7 @@ from tcas.models import Team, User
 class TeamSerializer(serializers.ModelSerializer):
     is_locked = serializers.BooleanField(read_only=True)
     leader = serializers.PrimaryKeyRelatedField(read_only=True)
+    members = serializers.PrimaryKeyRelatedField(many=True, read_only=False, queryset=User.objects.exclude(student_profile=None).all())
 
     class Meta:
         model = Team
