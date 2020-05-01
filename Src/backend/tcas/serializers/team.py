6a0185307fa711ed9301a9b1257678dd2d6732d3
Michael Long <mchobbylong@gmail.com>
2020-05-01 17:44:53 +0800

    set field `is_locked` in TeamSerializer as RO

--- Src/backend/tcas/serializers/team.py
+++ Src/backend/tcas/serializers/team.py
@@ -4,6 +4,7 @@ from tcas.models import Team, User
 
 
 class TeamSerializer(serializers.ModelSerializer):
+    is_locked = serializers.BooleanField(read_only=True)
     leader = serializers.PrimaryKeyRelatedField(read_only=True)
 
     class Meta:
